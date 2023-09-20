b55a392c5166e837ebe72a99cd941d3251d1766e
Quoc An <quocan050302@gmail.com>
2023-09-20 21:31:52 +0700

    Add Crypto App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Layout, Typography, Space } from 'antd'
+import { Link, Routes, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Navbar, Homepage, Exchanges, CryptoDetails, Cryptocurrencies, News } from './components'
+import './App.css'
+
+
+const App = () => {
+    return (
+        <div className='app'>
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path='/' element={<Homepage />}>
+                            </Route>
+                            <Route exact path='/exchanges' element={<Exchanges />}>
+                            </Route>
+                            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />}>
+                            </Route>
+                            <Route exact path='/crypto/:coinId' element={<CryptoDetails />}>
+                            </Route>
+                            <Route exact path='/news' element={<News />}>
+                            </Route>
+                        </Routes>
+                    </div>
+                </Layout>
+            <div className="footer">
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021 {" "}
+                        <Link to="/">
+                            Cryptoverse Inc.
+                        </Link> <br />
+                        All Rights Reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
